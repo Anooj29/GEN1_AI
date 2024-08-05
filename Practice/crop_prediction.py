7b431a8b1c54f3bd831c1a006362f53c3ffbe763
Anooj29 <154309295+Anooj29@users.noreply.github.com>
2024-08-06 01:15:23 +0530

    commit

---
+++ Practice/crop_prediction.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mlt
+import seaborn as sb 
+import sklearn as sk
+
+
+df = pd.read_csv("C:\GEN1_Artificial_Intelligence\Dataset\crops\yield.csv")
+print(df)
