c2eb6da80d0f2dc290d346da94bae7036980c788
Anooj29 <154309295+Anooj29@users.noreply.github.com>
2025-01-13 15:36:26 +0530

    commit

---
+++ Compe/call.py
@@ -0,0 +1,22 @@
+# Ask the bot to call a phone number
+data={
+  "bot_company": "Contoso",
+  "bot_name": "Amélie",
+  "phone_number": "+11234567890",
+  "task": "Help the customer with their digital workplace. Assistant is working for the IT support department. The objective is to help the customer with their issue and gather information in the claim.",
+  "agent_phone_number": "+917499437414",
+  "claim": [
+    {
+      "name": "hardware_info",
+      "type": "text"
+    },
+    {
+      "name": "first_seen",
+      "type": "datetime"
+    },
+    {
+      "name": "building_location",
+      "type": "text"
+    }
+  ]
+}
